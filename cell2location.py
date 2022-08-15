8a6ca2f33bf07320458943aa61d54f4fb19179d8
George <ggruenhagen3@gatech.edu>
2022-08-15 14:29:45 -0400

    cell2location

--- cell2location.py
+++ cell2location.py
@@ -1,6 +1,6 @@
 # *** MUST RUN THESE LINES *** #
-# conda activate cell2location
-# export PYTHONPATH="/storage/home/hcoda1/6/ggruenhagen3/p-js585-0/George/rich_project_pb1/conda_envs/cell2loc_e/python3.9/site-packages/"
+# conda activate cell2loc_env
+# export PYTHONPATH="/storage/home/hcoda1/6/ggruenhagen3/p-js585-0/George/rich_project_pb1/conda_envs/cell2loc_env/python3.9/site-packages/"
 # **************************** #
 
 import sys
@@ -21,10 +21,14 @@ run_name = f'{results_folder}/cell2location_map'
 # conda activate SeuratDisk
 # bb@reductions$pca = NULL
 # bb2 = Seurat::DietSeurat(bb, dimreducs = "umap")
+# DefaultAssay(all_merge) = "Spatial"
+# st = Seurat::DietSeurat(all_merge, dimreducs = "umap", assays = "Spatial")
 # SaveH5Seurat(bb2, filename = "/storage/home/hcoda1/6/ggruenhagen3/scratch/brain/data/bb.h5seurat")
 # Convert("/storage/home/hcoda1/6/ggruenhagen3/scratch/brain/data/bb.h5seurat", dest = "h5ad")
+# SaveH5Seurat(st, filename = "/storage/home/hcoda1/6/ggruenhagen3/scratch/brain/data/st_diet_070822.h5seurat")
+# Convert("/storage/home/hcoda1/6/ggruenhagen3/scratch/brain/data/st_diet_070822.h5seurat", dest = "h5ad")
 
-adata_vis = sc.read(f'/storage/home/hcoda1/6/ggruenhagen3/scratch/st/data/st_070822.h5ad')
+adata_vis = sc.read(f'/storage/home/hcoda1/6/ggruenhagen3/scratch/brain/data/st_diet_070822.h5ad')
 adata_ref = sc.read(f'/storage/home/hcoda1/6/ggruenhagen3/scratch/brain/data/bb.h5ad')
 
 """
@@ -51,27 +55,51 @@ mod.save(f"{ref_run_name}", overwrite=True)  # Save model
 adata_file = f"{ref_run_name}/bb_with_trained_model.h5ad"
 adata_ref.write(adata_file)
 
+# adata_ref = sc.read_h5ad(adata_file)
+# mod = cell2location.models.RegressionModel.load(f"{ref_run_name}", adata_ref)
+
 from matplotlib import pyplot as plt
 mod.plot_history(20)
 # plt.show()
 plt.savefig("/storage/home/hcoda1/6/ggruenhagen3/scratch/st/data/mod_train.png")
 plt.clf()
 
-"""
-Load the model:
-adata_file = f"{ref_run_name}/sc.h5ad"
-adata_ref = sc.read_h5ad(adata_file)
-mod = cell2location.models.RegressionModel.load(f"{ref_run_name}", adata_ref)
-"""
-# find shared genes and subset both anndata and reference signatures
-intersect = np.intersect1d(adata_vis.var_names, inf_aver.index)
-adata_vis = adata_vis[:, intersect].copy()
-inf_aver = inf_aver.loc[intersect, :].copy()
+mod.plot_QC()
+plt.savefig("/storage/home/hcoda1/6/ggruenhagen3/scratch/st/data/mod_train_qc.png")
+plt.clf()
 
+inf_aver = adata_ref.varm['means_per_cluster_mu_fg'][[f'means_per_cluster_mu_fg_{i}'
+                                                      for i in adata_ref.uns['mod']['factor_names']]].copy()
+inf_aver.columns = adata_ref.uns['mod']['factor_names']
+inf_aver.iloc[0:5, 0:5]
+inf_aver.to_csv("/storage/home/hcoda1/6/ggruenhagen3/scratch/st/data/bb_reference_signatures.csv")
+# inf_aver = pd.read_csv("/storage/home/hcoda1/6/ggruenhagen3/scratch/st/data/bb_reference_signatures.csv", index_col=0)
+
+"""
+Use the model on the Spatial data
+"""
 # prepare anndata for cell2location model
+adata_vis = adata_vis.raw.to_adata()
+adata_vis.X = adata_vis.X.toarray()
+adata_vis.var_names = inf_aver.index
 cell2location.models.Cell2location.setup_anndata(adata=adata_vis, batch_key="sample")
+del adata_vis.var
 
+mod_8_20  = cell2location.models.Cell2location(adata_vis, cell_state_df=inf_aver, N_cells_per_location=8, detection_alpha=20)
+mod_8_200 = cell2location.models.Cell2location(adata_vis, cell_state_df=inf_aver, N_cells_per_location=8, detection_alpha=200) # I think this the one we want
+mod_8_200.view_anndata_setup()
+mod_8_200.train(max_epochs=30000, batch_size=None, train_size=1, use_gpu=False)
+adata_vis = mod_8_200.export_posterior(adata_vis, sample_kwargs={'num_samples': 1000, 'batch_size': mod_8_200.adata.n_obs, 'use_gpu': False})
 
-mod_8_20 = cell2location.models.Cell2location(adata_vis, cell_state_df=inf_aver, N_cells_per_location=8, detection_alpha=20)
-mod_8_20 = cell2location.models.Cell2location(adata_vis, cell_state_df=inf_aver, N_cells_per_location=8, detection_alpha=200)
-mod.view_anndata_setup()
+adata_vis.obsm['q05_cell_abundance_w_sf'].to_csv("/storage/home/hcoda1/6/ggruenhagen3/scratch/st/data/cell2location_spatial_output_q05.csv")
+adata_vis.obsm['q95_cell_abundance_w_sf'].to_csv("/storage/home/hcoda1/6/ggruenhagen3/scratch/st/data/cell2location_spatial_output_q95.csv")
+adata_vis.obsm['means_cell_abundance_w_sf'].to_csv("/storage/home/hcoda1/6/ggruenhagen3/scratch/st/data/cell2location_spatial_output_means.csv")
+adata_vis.obsm['stds_cell_abundance_w_sf'].to_csv("/storage/home/hcoda1/6/ggruenhagen3/scratch/st/data/cell2location_spatial_output_stds.csv")
+
+mod_8_200.save(f"{run_name}", overwrite=True)
+adata_file = f"{run_name}/sp_trained.h5ad"
+adata_vis.write(adata_file)
+
+# model = torch.load("/storage/home/hcoda1/6/ggruenhagen3/scratch/st/data/cell2location_map/model.pt", map_location="cpu")
+# attr_dict = model["attr_dict"]
+# attr_dict["history_"]  # elbo_train
