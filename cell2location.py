73ce3e9510562246762ed5b6bcfd1d711cdd8793
George <ggruenhagen3@gatech.edu>
2022-12-05 14:46:35 -0500

    Cell2location B2

--- cell2location.py
+++ cell2location.py
@@ -15,8 +15,8 @@ import cell2location
 import scvi
 
 results_folder = "/storage/home/hcoda1/6/ggruenhagen3/scratch/st/data/"
-ref_run_name = f'{results_folder}/reference_signatures'
-run_name = f'{results_folder}/cell2location_map'
+ref_run_name = f'{results_folder}/reference_signatures_b2'
+run_name = f'{results_folder}/cell2location_map_b2'
 
 # conda activate SeuratDisk
 # bb@reductions$pca = NULL
@@ -28,7 +28,7 @@ run_name = f'{results_folder}/cell2location_map'
 # SaveH5Seurat(st, filename = "/storage/home/hcoda1/6/ggruenhagen3/scratch/brain/data/st_diet_070822.h5seurat")
 # Convert("/storage/home/hcoda1/6/ggruenhagen3/scratch/brain/data/st_diet_070822.h5seurat", dest = "h5ad")
 
-adata_vis = sc.read(f'/storage/home/hcoda1/6/ggruenhagen3/scratch/brain/data/st_diet_070822.h5ad')
+adata_vis = sc.read(f'/storage/home/hcoda1/6/ggruenhagen3/scratch/brain/data/st_b2_diet_120522.h5ad')
 adata_ref = sc.read(f'/storage/home/hcoda1/6/ggruenhagen3/scratch/brain/data/bb.h5ad')
 
 """
@@ -71,7 +71,7 @@ inf_aver = adata_ref.varm['means_per_cluster_mu_fg'][[f'means_per_cluster_mu_fg_
                                                       for i in adata_ref.uns['mod']['factor_names']]].copy()
 inf_aver.columns = adata_ref.uns['mod']['factor_names']
 inf_aver.iloc[0:5, 0:5]
-inf_aver.to_csv("/storage/home/hcoda1/6/ggruenhagen3/scratch/st/data/bb_reference_signatures15.csv")
+inf_aver.to_csv("/storage/home/hcoda1/6/ggruenhagen3/scratch/st/data/bb_b2_reference_signatures15.csv")
 # inf_aver = pd.read_csv("/storage/home/hcoda1/6/ggruenhagen3/scratch/st/data/bb_reference_signatures.csv", index_col=0)
 
 """
@@ -90,10 +90,10 @@ mod_8_200.view_anndata_setup()
 mod_8_200.train(max_epochs=30000, batch_size=None, train_size=1, use_gpu=False)
 adata_vis = mod_8_200.export_posterior(adata_vis, sample_kwargs={'num_samples': 1000, 'batch_size': mod_8_200.adata.n_obs, 'use_gpu': False})
 
-adata_vis.obsm['q05_cell_abundance_w_sf'].to_csv("/storage/home/hcoda1/6/ggruenhagen3/scratch/st/data/cell2location_spatial15_output_q05.csv")
-adata_vis.obsm['q95_cell_abundance_w_sf'].to_csv("/storage/home/hcoda1/6/ggruenhagen3/scratch/st/data/cell2location_spatial15_output_q95.csv")
-adata_vis.obsm['means_cell_abundance_w_sf'].to_csv("/storage/home/hcoda1/6/ggruenhagen3/scratch/st/data/cell2location_spatial15_output_means.csv")
-adata_vis.obsm['stds_cell_abundance_w_sf'].to_csv("/storage/home/hcoda1/6/ggruenhagen3/scratch/st/data/cell2location_spatial15_output_stds.csv")
+adata_vis.obsm['q05_cell_abundance_w_sf'].to_csv("/storage/home/hcoda1/6/ggruenhagen3/scratch/st/data/cell2location_b2_spatial15_output_q05.csv")
+adata_vis.obsm['q95_cell_abundance_w_sf'].to_csv("/storage/home/hcoda1/6/ggruenhagen3/scratch/st/data/cell2location_b2_spatial15_output_q95.csv")
+adata_vis.obsm['means_cell_abundance_w_sf'].to_csv("/storage/home/hcoda1/6/ggruenhagen3/scratch/st/data/cell2location_b2_spatial15_output_means.csv")
+adata_vis.obsm['stds_cell_abundance_w_sf'].to_csv("/storage/home/hcoda1/6/ggruenhagen3/scratch/st/data/cell2location_b2_spatial15_output_stds.csv")
 
 mod_8_200.save(f"{run_name}", overwrite=True)
 adata_file = f"{run_name}/sp_trained15.h5ad"
