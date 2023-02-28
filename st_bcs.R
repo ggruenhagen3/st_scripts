bfb291e5f86a0ce78d8ecce545ad1d25e3fce92e
George <ggruenhagen3@gatech.edu>
2023-02-28 09:12:11 -0500

    Better plotting

--- st_bcs.R
+++ st_bcs.R
@@ -270,8 +270,8 @@ nn_str      = ifelse(isNN, "nn_", "")
 sub1_str      = ifelse(isSub1, "sub1_", "")
 out_name = paste0("~/scratch/bcs/results/", cichlid_str, mouse.dataset, "_cluster_", glut_str, gaba_str, nn_str, sub1_str, "cor")
 if (out_name_overide != "") { out_name = out_name_overide }
-col.width = 2
-ggsave(paste0(out_name, ".pdf"), width = (ncol(mz.mouse.cor)/5) + col.width, height = nrow(mz.mouse.cor)/5)
+buffer.width = 2
+ggsave(paste0(out_name, ".pdf"), width = (ncol(mz.mouse.cor)/5) + buffer.width, height = (nrow(mz.mouse.cor)/5) + buffer.width)
 write.csv(mz.mouse.cor.maxed.out.melt, paste0(out_name, ".csv"))
 message(paste0("rclone copy ", paste0(out_name, ".pdf"), " dropbox:BioSci-Streelman/George/Brain/spatial/analysis/bcsm/", mouse.dataset))
 system( paste0("rclone copy ", paste0(out_name, ".pdf"), " dropbox:BioSci-Streelman/George/Brain/spatial/analysis/bcsm/", mouse.dataset))
