4ec732b613b68cbc2fb94a284b1d0a076fd02b87
ggruenhagen3 <ggruenhagen3@gatech.edu>
2023-03-08 12:37:09 -0500

    Neuronchat

---
+++ st_neuronchat.R
@@ -0,0 +1,92 @@
+# Read Input ===================================================================
+# this.run = 1; do.down = T; is.real = F; num.perms = 100;
+# this.run = 1; do.down = F; is.real = T; num.perms = 1; ind = 0;
+args = commandArgs(trailingOnly=TRUE)
+my.dataset  = as.character(args[1])
+meta.col = as.character(args[2])
+set.seed(1)
+message(paste0("Initializng run with: ", my.dataset))
+
+# Load Libraries ===============================================================
+options(stringsAsFactors = FALSE)
+source("~/scratch/bcs/bcs_scripts/bcs_f.R")
+suppressMessages(library('patchwork', quietly = T, warn.conflicts = F, verbose = F))
+suppressMessages(library('stringr',   quietly = T, warn.conflicts = F, verbose = F))
+suppressMessages(library('parallel',  quietly = T, warn.conflicts = F, verbose = F))
+suppressMessages(library('CellChat',  quietly = T, warn.conflicts = F, verbose = F))
+suppressMessages(library('NeuronChat',  quietly = T, warn.conflicts = F, verbose = F))
+
+
+# Load Data ====================================================================
+message("Loading Object...")
+gene_info = read.table("~/scratch/brain/cellchat/gene_info.txt", sep="\t", header = T, stringsAsFactors = F) 
+combined = readRDS("~/scratch/st/data/st_c2b2_hi_022023.rds")
+if (my.dataset == "st.sc") { stsc = readRDS() }
+message("Done.")
+
+# Human Object =================================================================
+# message("Creating a Human Object...")
+# mz.df = data.frame(mz = rownames(combined), human = gene_info$human[match(rownames(combined), gene_info$mzebra)])
+# mz.df$rowsums = rowSums(combined@assays$RNA@data)
+# mz.df = mz.df[order(-mz.df$rowsums),]
+# mz.df = mz.df[which(mz.df$rowsums != 0 & mz.df$human != "" & !is.na(mz.df$human)),]
+# mz.df$human[which(mz.df$human == "NRG2")] = "NRG3"
+# mz.df = rbind(data.frame(mz = "LOC101470250", human = "NRG2", rowsums = 5e6), mz.df)
+# 
+# mz.df = mz.df[!duplicated(mz.df$human),]
+mz.df = read.csv("~/scratch/brain/cellchat/bb_cc_gene_name_converter.csv")
+mz.df$X = NULL
+# data.input = as.matrix(combined@assays$RNA@data[mz.df$mz,])
+if (my.dataset == "st.sc") {
+  
+} else {
+  combined = NormalizeData(combined)
+  data.input = as.matrix(combined@assays$Spatial@data[mz.df$mz,])  
+}
+rownames(data.input) = mz.df$human
+message("Done.")
+
+# Cell Chat ====================================================================
+CellChatWeights = function(x) {
+  this.cells = colnames(data.input)
+  this.meta = data.frame(label = meta.label, row.names = this.cells)
+  
+  x <- createNeuronChat(normalized_count_mtx, DB='human',group.by = this.meta$label)
+  x <- run_NeuronChat(x,M=100)
+  net_aggregated_x <- net_aggregation(x@net,method = 'weight')
+  out.str = paste0("~/scratch/st/results/cellchat/neuronchat_", my.dataset, "_", meta.col, "_weights.rds")
+  saveRDS(net_aggregated_x, out.str)
+  
+  return(net_aggregated_x)
+}
+
+# Getting correct labels
+if (meta.col == "ct"        && my.dataset != "stsc") { meta.label = as.vector(combined@meta.data[, meta.col]) }
+if (meta.col == "structure" && my.dataset != "stsc") { meta.label = combined@meta.data[, meta.col] }
+
+rm(combined) # delete original Seurat object to save memory
+
+message("Running cellchat (this while take awhile)...")
+num.parallel.jobs = 1
+message(paste0("Using ", num.parallel.jobs, " cores."))
+sink(file="~/scratch/brain/cellchat_sink.txt")
+run_outs = CellChatWeights(x)
+sink()
+
+n.success = length(run_outs)
+# if (n.success != num.perms) { message(paste0("Not all runs were successful (", (num.perms - n.success), "/", num.perms, ")")) }
+# out = as.data.frame(do.call('cbind', run_outs))
+# colnames(out) = paste0("run", 1:n.success)
+# out[, c("clust1", "clust2")] = reshape2::colsplit(names(run_outs[[1]]), "\\.", c("1", "2"))
+# out = out[, c(n.success+1, n.success+2, 1:n.success)]
+out = run_outs
+message("Done.")
+
+# Save Output ==================================================================
+message("Writing Output...")
+todays.date = stringr::str_split(Sys.Date(), pattern = "-")[[1]]
+todays.date = paste0(todays.date[2], todays.date[3], substr(todays.date[1], 3, 4))
+out.str = paste0("~/scratch/st/results/cellchat/cellchat_", my.dataset, "_", meta.col, "_weights.csv")
+write.csv(out, out.str)
+message("Done.")
+message("All Done.")
